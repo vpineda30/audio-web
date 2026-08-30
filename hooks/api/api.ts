09a55b6d48ed9759230200fd11259398b80e3973
vpineda30 <vinicius.pineda.senai404@gmail.com>
2026-08-29 22:44:29 -0300

    feat: update API_URL to use NEXT_PUBLIC_API_URL environment variable

--- hooks/api/api.ts
+++ hooks/api/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
     ?? (typeof window !== "undefined"
         ? `${window.location.protocol}//${window.location.hostname}:3000`
         : "http://localhost:3000");
